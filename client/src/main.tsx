be3ae5baf0c463ec52c7ae9d5e38befa9d89aaa9
Debarup Sarkar <debarup24sarkar@gmail.com>
2026-05-28 20:44:43 +0530

    server url add

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AppProvider } from "./context/AppContext.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-export const server = "http://localhost:5000";
+export const server = "https://careerai-220z.onrender.com";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
